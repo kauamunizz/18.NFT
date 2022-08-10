b1aaa339377832fa6ee2dc06ae05f5517916d415
Kauã Muniz <kauamunizz@hotmail.com>
2022-08-09 21:44:49 -0300

    atualizacao

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
 import '../styles/styles.scss';
+
+$(function (){
+    $(".carousel-items").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: $(".back-to"),
+        nextArrow: $(".next-to"),
+        centerPadding: '60px',
+        variableWidth: true,
+    })
+})
