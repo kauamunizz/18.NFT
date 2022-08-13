b4195fe501a47f60b143751b13e57d962b18090d
Kauã Muniz <kauamunizz@hotmail.com>
2022-08-12 22:59:19 -0300

    cards

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -27,3 +27,31 @@ document.querySelectorAll('.controle').forEach(button => {
         document.querySelector(dataBtn).style.display = 'grid';
     })
 })
+
+
+function createCard(card) {
+    const {image, name, price, author} = card;
+
+    return /*html*/`
+        <div class="polaroide">
+            <div >
+                <img class="galeria-imagem" src="${image}">
+            </div>
+            <div class="infos-bloco">
+                <div class="infos">
+                    <h3>${name}</h3>
+                    <p>${author}</p>
+                </div>
+                <div class="price">
+                    <h4>${price} ETH</h4>
+                </div>
+            </div>
+            <button class="button-market buy" type="button">Collect Now</button>
+        </div>
+    `;
+}
+
+POLAROIDE.forEach(card => {
+    document.querySelector('.galeria-market')
+    .insertAdjacentHTML('afterbegin', createCard(card));
+});
