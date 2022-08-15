3195b0094b4f28f54d80b3b22d354317e65deee4
Kauã Muniz <kauamunizz@hotmail.com>
2022-08-15 18:00:06 -0300

    correcao

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,25 +1,25 @@
 export const CAROUSEL = [
     {
-        image: '/src/assets/imgs/bloco1-bg.png',
-        logo: '/src/assets/imgs/bloco1-logo.png',
+        image: '/imgs/bloco1-bg.png',
+        logo: '/imgs/bloco1-logo.png',
         name: 'Hibnastiar',
         followers: '27.3K'
     },
     {
-        image: '/src/assets/imgs/bloco2-bg.png',
-        logo: '/src/assets/imgs/bloco2-logo.png',
+        image: '/imgs/bloco2-bg.png',
+        logo: '/imgs/bloco2-logo.png',
         name: 'Tukirin',
         followers: '21.6K'
     },
     {
-        image: '/src/assets/imgs/bloco3-bg.png',
-        logo: '/src/assets/imgs/bloco3-logo.png',
+        image: '/imgs/bloco3-bg.png',
+        logo: '/imgs/bloco3-logo.png',
         name: 'Aksaraz',
         followers: '14.0K'
     },
     {
-        image: '/src/assets/imgs/bloco4-bg.png',
-        logo: '/src/assets/imgs/bloco4-logo.png',
+        image: '/imgs/bloco4-bg.png',
+        logo: '/imgs/bloco4-logo.png',
         name: 'Yuk Jum',
         followers: '11.9K'
     }
