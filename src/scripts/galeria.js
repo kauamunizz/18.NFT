b86e698c4050296892bb68ce52e722cad16f8082
Kauã Muniz <kauamunizz@hotmail.com>
2022-08-15 23:31:11 -0300

    images

--- src/scripts/galeria.js
+++ src/scripts/galeria.js
@@ -1,24 +1,24 @@
 export const GALERIA = [
     {
-        image: 'http://127.0.0.1:5173/imgs/galeria-1.png',
+        image: '/imgs/galeria-1.png',
         name: 'Miles Grogosters',
         price: 4.99,
         author: '@rominolegs',
     },
     {
-        image: 'http://127.0.0.1:5173/imgs/galeria-2.png',
+        image: '/imgs/galeria-2.png',
         name: 'Reox Fancxy',
         price: 3.27,
         author: '@hibnastiar',
     },
     {
-        image: 'http://127.0.0.1:5173/imgs/galeria-3.png',
+        image: '/imgs/galeria-3.png',
         name: 'Miles Grogosters',
         price: 4.20,
         author: '@senjadisini',
     },
     {
-        image: 'http://127.0.0.1:5173/imgs/galeria-4.png',
+        image: '/imgs/galeria-4.png',
         name: 'Tuinz Rey',
         price: 2.99,
         author: '@albertov',
