0cce31e30c246e14a6775eb6de0f03f48c44fd2b
Kauã Muniz <kauamunizz@hotmail.com>
2022-08-30 11:16:25 -0300

    compact

--- src/data/carousel.js
+++ src/data/carousel.js
@@ -1,25 +1,25 @@
 export const CAROUSEL = [
     {
-        image: '/imgs/bloco1-bg.png',
-        logo: '/imgs/bloco1-logo.png',
+        image: '/imgs/bloco1-bg.png?v=2',
+        logo: '/imgs/bloco1-logo.png?v=2',
         name: 'Hibnastiar',
         followers: '27.3K'
     },
     {
-        image: '/imgs/bloco2-bg.png',
-        logo: '/imgs/bloco2-logo.png',
+        image: '/imgs/bloco2-bg.png?v=2',
+        logo: '/imgs/bloco2-logo.png?v=2',
         name: 'Tukirin',
         followers: '21.6K'
     },
     {
-        image: '/imgs/bloco3-bg.png',
-        logo: '/imgs/bloco3-logo.png',
+        image: '/imgs/bloco3-bg.png?v=2',
+        logo: '/imgs/bloco3-logo.png?v=2',
         name: 'Aksaraz',
         followers: '14.0K'
     },
     {
-        image: '/imgs/bloco4-bg.png',
-        logo: '/imgs/bloco4-logo.png',
+        image: '/imgs/bloco4-bg.png?v=2',
+        logo: '/imgs/bloco4-logo.png?v=2',
         name: 'Yuk Jum',
         followers: '11.9K'
     }
--- src/data/galeria.js
+++ src/data/galeria.js
@@ -1,24 +1,24 @@
 export const GALERIA = [
     {
-        image: '/imgs/galeria-1.png',
+        image: '/imgs/galeria-1.png?v=2',
         name: 'Miles Grogosters',
         price: 4.99,
         author: '@rominolegs',
     },
     {
-        image: '/imgs/galeria-2.png',
+        image: '/imgs/galeria-2.png?v=2',
         name: 'Reox Fancxy',
         price: 3.27,
         author: '@hibnastiar',
     },
     {
-        image: '/imgs/galeria-3.png',
+        image: '/imgs/galeria-3.png?v=2',
         name: 'Miles Grogosters',
         price: 4.20,
         author: '@senjadisini',
     },
     {
-        image: '/imgs/galeria-4.png',
+        image: '/imgs/galeria-4.png?v=2',
         name: 'Tuinz Rey',
         price: 2.99,
         author: '@albertov',
